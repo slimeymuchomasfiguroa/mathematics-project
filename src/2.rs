c341f7e1d219935215917565a61fbf7a308d1240
slimeymuchomasfiguroa <slimeymuchomasfiguroa@gmail.com>
2025-03-06 18:34:56 +0000

    Add 2.rs

---
+++ src/2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 10;
+    if x > 5 {
+        println!("{} is greater than 5", x);
+    } else if x < 5 {
+        println!("{} is less than 5", x);
+    } else {
+        println!("{} equals 5", x);
+    }
+}
