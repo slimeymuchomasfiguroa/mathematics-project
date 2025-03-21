3a1c77a7320bd38d16f7acbefeea3420e82622d9
slimeymuchomasfiguroa <slimeymuchomasfiguroa@gmail.com>
2025-03-21 06:54:58 +0000

    Add 16.rs

---
+++ src/16.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // Example Rust code 1
+    let x = 5;
+    let y = 3;
+    let result = x + y;
+    println!("The sum of {} and {} is {}", x, y, result);
+
+    // Example Rust code 2
+    let a = 10.5;
+    let b = 4.2;
+    let result = a / b;
+    println!("The division of {} and {} is {}", a, b, result);
+}
+
