d7864e458c43514081ffec8ae2e3a4fb77196abd
slimeymuchomasfiguroa <slimeymuchomasfiguroa@gmail.com>
2025-04-08 22:07:17 +0000

    Add 27.rs

---
+++ src/27.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x > 0 {
+        println!("x is positive.");
+    } else if x == 0 {
+        println!("x is zero.");
+    } else {
+        println!("x is negative.");
+    }
+}
